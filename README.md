1265d9056f2cf218fc590de0cdd967e553488671
shiguanghai <1932275668@qq.com>
2021-03-28 22:19:40 +0800

    模板及样式 初始化

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# ToDoList
-Vue 3.0 based todolist
+# todolist
+
+## Project setup
+
+```shell
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```shell
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```shell
+npm run build
+```
+
+### Lints and fixes files
+
+```shell
+npm run lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <section id="app" class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input
+        class="new-todo"
+        placeholder="What needs to be done?"
+        autocomplete="off"
+        autofocus
+        >
+    </header>
+    <section class="main">
+      <input id="toggle-all" class="toggle-all" type="checkbox">
+      <label for="toggle-all">Mark all as complete</label>
+      <ul class="todo-list">
+        <li>
+          <div class="view">
+            <input class="toggle" type="checkbox">
+            <label >测试数据</label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" type="text">
+        </li>
+        <li>
+          <div class="view">
+            <input class="toggle" type="checkbox">
+            <label >测试数据</label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" type="text">
+        </li>
+      </ul>
+    </section>
+    <footer class="footer">
+      <span class="todo-count">
+        <strong>1</strong> item left
+      </span>
+      <ul class="filters">
+        <li><a href="#/all">All</a></li>
+        <li><a href="#/active">Active</a></li>
+        <li><a href="#/completed">Completed</a></li>
+      </ul>
+      <button class="clear-completed">
+        Clear completed
+      </button>
+    </footer>
+  </section>
+  <footer class="info">
+    <p>Double-click to edit a todo</p>
+    <!-- Remove the below line ↓ -->
+    <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
+    <!-- Change this out with your name and url ↓ -->
+    <p>Created by <a href="https://shiguanghai.top">shiguanghai</a></p>
+    <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+  </footer>
+</template>
+
+<script>
+import './assets/index.css'
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
