0c75039a591870d271ea01f5435a3548d5e2d6cb
shiguanghai <1932275668@qq.com>
2021-05-12 14:46:02 +0800

    最简单的方法测试组件

---
+++ src/components/Gretting.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Gretting',
+  data () {
+    return {
+      message: 'Hello World'
+    }
+  }
+}
+</script>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -11,9 +11,27 @@
 //   })
 // })
 
-import { sum } from '@/utils/cal'
+// import { sum } from '@/utils/cal'
+import Gretting from '@/components/Gretting'
+import { createApp } from 'vue'
 
 // it
-test('first test', () => {
-  expect(sum(1, 2)).toBe(3)
+// test('first test', () => {
+//   expect(sum(1, 2)).toBe(3)
+// })
+
+test('Gretting', () => {
+  // 你可以想象这里有个空页面
+  // 测试环境有一个虚拟的 DOM 环境
+  // 内部用的 jsdom 来模拟浏览器 DOM
+  const root = document.createElement('div')
+  root.id = 'app'
+  document.body.appendChild(root)
+
+  // 把 Vue 组件渲染到 HTML 元素节点上
+  createApp(Gretting).mount('#app')
+
+  // console.log(document.body.innerHTML)
+  const title = document.querySelector('h1')
+  expect(title.innerHTML).toBe('Hello World')
 })
