0019aa60cb59f8cb74a7aaf0c7858e6ad95ec86a
shiguanghai <1932275668@qq.com>
2021-06-16 16:49:32 +0800

    测试开发头部组件

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -2,18 +2,27 @@
   <header class="header">
     <h1 data-testid="header-title">todos</h1>
     <input
+      data-testid="new-todo-input"
       class="new-todo"
       placeholder="What needs to be done?"
-      autocomplete="off"
       autofocus
-      v-model="input"
-      @keyup.enter="addTodo"
-      >
+      @keyup.enter="handleNewTodo"
+    />
   </header>
 </template>
 
 <script>
 export default {
-  name: 'TodoHeader'
+  name: 'TodoHeader',
+  methods: {
+    handleNewTodo (e) {
+      const { value } = e.target
+      if (!value.trim().length) {
+        return
+      }
+      this.$emit('new-todo', value)
+      e.target.value = '' // 清空文本框
+    }
+  }
 }
 </script>
--- tests/unit/TodoHeader.spec.js
+++ tests/unit/TodoHeader.spec.js
@@ -10,4 +10,27 @@ describe('TodoHeader', () => {
     const title = wrapper.find('[data-testid="header-title"]')
     expect(title.text()).toBe('todos')
   })
+
+  test('添加任务，输入有效数据发布 new-todo 事件', async () => {
+    const wrapper = shallowMount(TodoHeader)
+    const newTodoInput = wrapper.find('[data-testid="new-todo-input"]')
+    const text = 'hello'
+    newTodoInput.setValue(text)
+    // 触发回车事件
+    await newTodoInput.trigger('keyup.enter')
+    // 断言
+    expect(wrapper.emitted()['new-todo']).toBeTruthy() // 对外发布了 new-todo 事件
+    expect(wrapper.emitted()['new-todo'][0][0]).toBe(text) // new-todo 事件参数必须是 hello
+    expect(newTodoInput.element.value).toBe('') // 发布完文本框被清空
+  })
+
+  test('添加任务，输入无效数据，不会往外发布 new-todo 事件', async () => {
+    const wrapper = shallowMount(TodoHeader)
+    const newTodoInput = wrapper.find('[data-testid="new-todo-input"]')
+    newTodoInput.setValue('')
+    // 触发回车事件
+    await newTodoInput.trigger('keyup.enter')
+    // 断言
+    expect(wrapper.emitted()['new-todo']).toBeFalsy() // 不对外发布 new-todo 事件
+  })
 })
